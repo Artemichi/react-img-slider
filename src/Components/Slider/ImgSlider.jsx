deb4570570c2933d218f5a3d9946d66fd819b69e
Artemichi <55092560+Artemichi@users.noreply.github.com>
2020-04-11 21:06:10 +0500

    animation -> controls and indicators

--- src/Components/Slider/ImgSlider.jsx
+++ src/Components/Slider/ImgSlider.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import classes from './imgSlider.module.css'
 import reducer from './imgSliderReducer'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import Indicators from './Indicators';
+import { Motion, spring } from 'react-motion'
 
 const ImgSlider = () => {
 	const [images] = React.useState([
@@ -13,19 +14,32 @@ const ImgSlider = () => {
 		'https://img5.goodfon.ru/original/1920x1080/0/5d/lisa-ryzhaia-morda-profil-portret-fon.jpg'
 	])
 
-	const [idx, dispatch] = React.useReducer(reducer, 0)
+	const [idx, dispatch] = useReducer(reducer, 0)
 
 	return (
 		<React.Fragment>
-			<div className={classes.carusel} style={{
-				backgroundImage: `url(${images[idx]})`,
-			}}>
-				<button className={`${classes.control} ${classes.prev}`} onClick={() => dispatch({ type: 'PREV_IMG', payload: { index: idx, last: images.length - 1 } })}>
-					<ArrowBackIcon fontSize='large' color='action' />
-				</button>
-				<button className={`${classes.control} ${classes.next}`} onClick={() => dispatch({ type: 'NEXT_IMG', payload: images.length })}>
-					<ArrowForwardIcon fontSize='large' color='action' />
-				</button>
+			{/* image */}
+			<div className={classes.carusel} style={{ backgroundImage: `url(${images[idx]})` }}>
+				{/* controls */}
+				<Motion defaultStyle={{ x: -200 }} style={{ x: spring(0, { stiffness: 100 }) }}>
+					{(style) => (
+						<button style={{ transform: `translateX(${style.x}px)` }}
+							className={`${classes.control} ${classes.prev}`}
+							onClick={() => dispatch({ type: 'PREV_IMG', payload: { index: idx, last: images.length - 1 } })}>
+							<ArrowBackIcon fontSize='large' color='action' />
+						</button>
+					)}
+				</Motion>
+				<Motion defaultStyle={{ x: 200 }} style={{ x: spring(0, { stiffness: 100 }) }}>
+					{(style) => (
+						<button style={{ transform: `translateX(${style.x}px)` }}
+							className={`${classes.control} ${classes.next}`}
+							onClick={() => dispatch({ type: 'NEXT_IMG', payload: images.length })}>
+							<ArrowForwardIcon fontSize='large' color='action' />
+						</button>
+					)}
+				</Motion>
+				{/* indicators */}
 				<Indicators imgs={images} index={idx} dispatch={dispatch} />
 			</div>
 		</React.Fragment>
--- src/Components/Slider/Indicators.jsx
+++ src/Components/Slider/Indicators.jsx
@@ -2,26 +2,33 @@ import React from 'react'
 import classes from './imgSlider.module.css'
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+import { Motion, spring } from 'react-motion'
 
 const Indicators = ({ imgs, index, dispatch }) => {
 	return (
 		<React.Fragment>
-			<ul className={classes.indicatorsList}>
+			<Motion defaultStyle={{ opacity: 0, y: 200 }} style={{ opacity: spring(1, { stiffness: 30 }), y: spring(0, { stiffness: 100 }) }}>
 				{
-					imgs.map((e, i) => {
-						return (
-							<li key={i} onClick={() => dispatch({ type: 'JUMP_TO', payload: i })}>
-								{
-									i === index ?
-										<RadioButtonCheckedIcon color='inherit' fontSize='small' />
-										:
-										<RadioButtonUncheckedIcon color='action' fontSize='small' />
-								}
-							</li>
-						)
-					})
+					(style) => (
+						<ul className={classes.indicatorsList} style={{ opacity: style.opacity, transform: `translateY(${style.y}px)` }}>
+							{
+								imgs.map((e, i) => {
+									return (
+										<li key={i} onClick={() => dispatch({ type: 'JUMP_TO', payload: i })}>
+											{
+												i === index ?
+													<RadioButtonCheckedIcon color='inherit' fontSize='small' />
+													:
+													<RadioButtonUncheckedIcon color='action' fontSize='small' />
+											}
+										</li>
+									)
+								})
+							}
+						</ul>
+					)
 				}
-			</ul>
+			</Motion>
 		</React.Fragment>
 	)
 }

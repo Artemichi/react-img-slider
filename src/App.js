118eed7d6d88bec372c11876c1bec4e9ed43112e
Artemichi <55092560+Artemichi@users.noreply.github.com>
2020-04-12 01:24:06 +0500

    moved images state up

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ImgSlider from './Components/Slider/ImgSlider'
 
 function App() {
+  const [images] = React.useState([
+    'https://img5.goodfon.ru/original/1920x1080/d/d3/suslik-rastitelnost-fon-morda.jpg',
+    'https://img2.goodfon.ru/original/1920x1080/1/9f/golub-ptitsa-progulka.jpg',
+    'https://img5.goodfon.ru/original/1920x1080/9/b9/ptitsa-tupik-portret-fon-atlanticheskii.jpg',
+    'https://img5.goodfon.ru/original/1920x1080/0/5d/lisa-ryzhaia-morda-profil-portret-fon.jpg',
+  ])
+
   return (
     <div className='App'>
-      <ImgSlider />
+      <ImgSlider images={images} />
     </div>
   )
 }
--- src/Components/Slider/ImgSlider.jsx
+++ src/Components/Slider/ImgSlider.jsx
@@ -6,14 +6,7 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import Indicators from './Indicators'
 import { Motion, spring } from 'react-motion'
 
-const ImgSlider = () => {
-  const [images] = useState([
-    'https://img5.goodfon.ru/original/1920x1080/d/d3/suslik-rastitelnost-fon-morda.jpg',
-    'https://img2.goodfon.ru/original/1920x1080/1/9f/golub-ptitsa-progulka.jpg',
-    'https://img5.goodfon.ru/original/1920x1080/9/b9/ptitsa-tupik-portret-fon-atlanticheskii.jpg',
-    'https://img5.goodfon.ru/original/1920x1080/0/5d/lisa-ryzhaia-morda-profil-portret-fon.jpg',
-  ])
-
+const ImgSlider = ({ images }) => {
   const [showControls, setShowControls] = useState(true)
 
   const [idx, dispatch] = useReducer(reducer, 0)
